0002f35cbe7cf68b94a245928dfefb6b84c8f5bf
ike04 <boc.0409@outlook.jp>
2020-10-10 12:30:11 +0900

    登録画面の入力部分の実装

--- RealmSample/ViewController.swift
+++ RealmSample/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameTextField: UITextField!  {
+        didSet {
+            userNameTextField.placeholder = "ユーザー名"
+            userNameTextField.clearButtonMode = .always
+        }
+    }
+    @IBOutlet weak var userAgeTextField: UITextField!{
+        didSet {
+            userAgeTextField.placeholder = "年齢"
+            userAgeTextField.clearButtonMode = .always
+        }
+    }
+    @IBOutlet weak var registerButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
 }
+
 
